aa0833ab574694cfcd1861243a5954360dc82b72
Tallmatra <trawaremamadou@gmail.com>
2022-03-01 16:17:32 +0000

    m

---
+++ config/contantes.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * $_server['script_FILENAME'] signifie tout le chemin qui méne vers le chemein  
+*Chemin sur dossier racine du projet
+*/
+define("ROOT",str_replace("public".DIRECTORY_SEPARATOR."index.php","",$_SERVER['SCR
+IPT_FILENAME']));
+
+/**
+* Chemin sur dossier src qui contient les controllers et les modeles
+*/
+define("PATH_SRC",ROOT."src".DIRECTORY_SEPARATOR);
+/**
+* Chemin sur dossier templates du projet
+*/
+define("PATH_VIEWS",ROOT."templates".DIRECTORY_SEPARATOR);
+/**
+* Chemin sur data qui contient le fichier Json db.json
+*/
+define("PATH_DB",ROOT."data".DIRECTORY_SEPARATOR."db.json");
+/**
+* Chemin sur le dossier public , pour inclusion des images,css et js
+ */  
+define("WEB_ROOT","http://localhost:8002/");
---
+++ src/controllers/securite.controllers.php
@@ -0,0 +1,22 @@
+<?php
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+   if(isset($_REQUEST['action'])){
+     if(($_REQUEST['action']=="connexion")){
+
+      }
+
+    }
+
+}
+
+
+if($_SERVER["REQUEST_METHOD"]=="GET"){
+ 
+if(isset($_REQUEST['action'] )){
+  if(($_REQUEST['action']=="connexion")){
+
+  }
+    
+}
+
+}
